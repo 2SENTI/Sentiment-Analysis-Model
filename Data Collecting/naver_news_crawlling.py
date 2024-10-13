5483ebe4968c22c8aa266447340521aee6c30efd
MyeongJuCho <155465796+MyeongJuCho@users.noreply.github.com>
2024-10-13 21:15:05 +0900

    [Refactor] Change the data collection period
    Begin data crawling from January 1, 2020

--- Data Collecting/naver_news_crawlling.py
+++ Data Collecting/naver_news_crawlling.py
@@ -20,20 +20,19 @@ import time
 service = Service('C:\\Program Files\\chromedriver.exe')
 driver = webdriver.Chrome(service=service)
 
-
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
 
 def text_clean(text):
-    pattern = r'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)' # E-mail제거
+    pattern = r'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)' # E-mail 제거
     text = re.sub(pattern, '', text)
-    pattern = r'(http|ftp|https)://(?:[-\w.]|(?:%[\da-fA-F]{2}))+' # URL제거
+    pattern = r'(http|ftp|https)://(?:[-\w.]|(?:%[\da-fA-F]{2}))+' # URL 제거
     text = re.sub(pattern, '', text)
     pattern = '<[^>]*>'         # HTML 태그 제거
     text = re.sub(pattern, '', text)
-    pattern = '[\n]'            # \n제거
+    pattern = '[\n]'            # \n 제거
     text = re.sub(pattern, '', text)
-    pattern = '[\t]'            # \n제거
+    pattern = '[\t]'            # \t 제거
     text = re.sub(pattern, '', text)
     pattern = '[\']'           
     text = re.sub(pattern, '', text)
@@ -41,9 +40,6 @@ def text_clean(text):
     text = re.sub(pattern, '', text)
     return text  
 
-
-# 데이터베이스에 index가 어디까지 저장돼있는지 확인하는 함수 필요함
-# db에 저장될때 인덱스 중복 방지
 def checkMaxNumber() :
     conn = pymysql.connect(
         host='hostname'
@@ -55,159 +51,142 @@ def checkMaxNumber() :
     cur = conn.cursor()
 
     try:
-        # 쿼리 실행
         cur.execute('SELECT MAX(Number) AS maxNumber FROM news')
-        # 결과 가져오기
         results = cur.fetchall()
-        # 결과 출력 및 MaxNumber 변수에 저장
         if results:
-            MaxNumber = results[0][0]  # 첫 번째 행의 첫 번째 컬럼 값을 가져옴
-            return(MaxNumber)  # MaxNumber 값을 출력
+            MaxNumber = results[0][0]  
+            return(MaxNumber)  
         else:
             return(0)
     except Exception as e :
-            print('Error Message :', e)
-            pass
+        print('Error Message :', e)
+        pass
     finally:
-        # 커서와 연결을 닫음
         cur.close()
         conn.close()
 
-# db에 기사가 몇개까지 저장됐는지
-# save_articleCnt=checkMaxNumber()
 save_articleCnt=0
 
-x=dt.datetime.now()
-dateCnt=0 # 가져올 날짜범위(개수)
-date=int(x.strftime("%Y%m%d"))-1
-urlList=[]
-num=0
+# 현재 날짜와 원하는 날짜 범위를 설정 (예: 2020년 1월 1일까지)
+x = dt.datetime.now()
+end_date = dt.datetime(2020, 1, 1)  # 2020년 1월 1일까지 크롤링
+date = x
 
-articleCnt=0 # 크롤링할 기사 개수
-images=[]
-addresses=[]
-titles=[]
-nums=[]
-presses=[]
-wdates=[]
-articleURLs=[]
-summarys=[]
-img_list=[]
+urlList = []
+num = 0
 
-while articleCnt<30 : # 기사 모든 날짜 출력(지정한 개수의 날짜만큼만)
-    url=f"https://finance.naver.com/news/news_list.naver?mode=LSS3D&section_id=101&section_id2=258&section_id3=402&date={date}"
+articleCnt = 0
+images = []
+addresses = []
+titles = []
+nums = []
+presses = []
+wdates = []
+articleURLs = []
+contents = []
+img_list = []
+
+while date >= end_date:  # 2020년 1월 1일까지 크롤링
+    date_str = date.strftime("%Y%m%d")  # YYYYMMDD 형식으로 날짜 출력
+    url = f"https://finance.naver.com/news/news_list.naver?mode=LSS3D&section_id=101&section_id2=258&section_id3=402&date={date_str}"
     res = req.urlopen(url).read().decode('cp949') 
     soup = BeautifulSoup(res, "html.parser")
 
-    #각 날짜를 기준으로 바뀌는 영역
-    tr=soup.find('tr',attrs={'align':'center'})
-    tds=tr.find_all('td') #첫번째 페이지를 기준으로 해서 td의 갯수를 센다 
-    tdsCnt=len(tds)-1 # 맨뒤를 담는 td태그는 갯수에서 제외
-    if tdsCnt==0 : tdsCnt=1 # 1페이지만 있는 경우는 맨앞, 맨뒤가 존재하지 않음
+    tr = soup.find('tr', attrs={'align':'center'})
+    tds = tr.find_all('td')
+    tdsCnt = len(tds) - 1
+    if tdsCnt == 0: 
+        tdsCnt = 1
     print("\n")
 
-    #날짜에 각 페이지를 기준으로 바뀌는 영역
-    cnt=1
-    print('---------------------------------------------',date,' 뉴스 링크---------------------------------------------------')
+    cnt = 1
+    print('---------------------------------------------', date_str, ' 뉴스 링크---------------------------------------------------')
     print("\n")
 
-    while cnt<=tdsCnt : # 하루치 기사만 리스트에 저장
-        ur= f"https://finance.naver.com/news/news_list.naver?mode=LSS3D&section_id=101&section_id2=258&section_id3=402&date={date}&page={cnt}"
+    while cnt <= tdsCnt:
+        ur = f"https://finance.naver.com/news/news_list.naver?mode=LSS3D&section_id=101&section_id2=258&section_id3=402&date={date_str}&page={cnt}"
         
-        # 기사 url 따로 리스트에 저장
-        urlList.insert(num,ur)
-        cnt=cnt+1
-        num=num+1
-        res = req.urlopen(ur).read().decode('cp949') 
+        urlList.insert(num, ur)
+        cnt += 1
+        num += 1
+        res = req.urlopen(ur).read().decode('cp949')
         soup = BeautifulSoup(res, "html.parser")
-        NewsList=soup.select('#contentarea_left > ul.realtimeNewsList')
+        NewsList = soup.select('#contentarea_left > ul.realtimeNewsList')
 
-        for linkList in NewsList: # 한 페이지의 기사만 리스트에 저장
-            links=linkList.select('li')
-            for link in links :    
-                articleSubjects=link.select('.articleSubject')
-                articleSummarys=link.select('.articleSummary')
+        for linkList in NewsList:
+            links = linkList.select('li')
+            for link in links:    
+                articleSubjects = link.select('.articleSubject')
+                articleSummarys = link.select('.articleSummary')
 
-                for articleSubject in articleSubjects :
-                    title=articleSubject.select_one('a').get_text()
-                    address=articleSubject.select_one('a').get('href')
+                for articleSubject in articleSubjects:
+                    title = articleSubject.select_one('a').get_text()
+                    address = articleSubject.select_one('a').get('href')
 
-                    title=text_clean(title)
+                    title = text_clean(title)
                     titles.append(title)
                     addresses.append(address)
-                    articleCnt=articleCnt+1 #기사 개수 카운트
-                    nums.append(articleCnt+save_articleCnt)
-                    print(articleCnt+save_articleCnt, title)
+                    articleCnt += 1 
+                    nums.append(articleCnt + save_articleCnt)
+                    print(articleCnt + save_articleCnt, title)
 
-                for articleSummary in articleSummarys :
-                    press=articleSummary.select_one('.press').get_text()
-                    wdate=articleSummary.select_one('.wdate').get_text()
+                for articleSummary in articleSummarys:
+                    press = articleSummary.select_one('.press').get_text()
+                    wdate = articleSummary.select_one('.wdate').get_text()
                     presses.append(press)
-                    wdates.append(wdate)  
-    dateCnt=dateCnt+1
-    date=date-1
+                    wdates.append(wdate)
 
-#위의 반복문에서 기간동안의 기사를 모두 리스트에 저장 후 해당 기사 개수만큼 출력
-k=0
-for k in range(articleCnt) :
-    # print("\n\n\nNo.",nums[k]+save_articleCnt, "\ntitle : ",titles[k],
-    #         "\npress : " ,presses[k],"\twdate : ",wdates[k])
+    date -= dt.timedelta(days=1)  # 하루씩 줄이기
 
-    a=addresses[k].find('article_id=')#o위치-1 출력
-    b=addresses[k].find('&office_id')#&위치-1 출력
-    c=addresses[k].find('&mode')#&위치-1 출력
-    article_id=addresses[k]
-    article_id=article_id[a+11:b]
-    office_id=addresses[k]
-    office_id=office_id[b+11:c]                
+k = 0
+for k in range(articleCnt):
+    a = addresses[k].find('article_id=')
+    b = addresses[k].find('&office_id')
+    c = addresses[k].find('&mode')
+    article_id = addresses[k]
+    article_id = article_id[a+11:b]
+    office_id = addresses[k]
+    office_id = office_id[b+11:c]                
     
-    articleURL=f"https://n.news.naver.com/mnews/article/{office_id}/{article_id}"
+    articleURL = f"https://n.news.naver.com/mnews/article/{office_id}/{article_id}"
 
-    # requests로 URL 먼저 가져오기
-    res=requests.get(articleURL)
+    res = requests.get(articleURL)
 
-    # URL을 리스트에 추가
     articleURLs.append(articleURL)
 
-    # Selenium으로 해당 URL 열기 (새 창이 열리지 않도록)
     driver.get(articleURL)
 
     try:
-        # URL 로딩 대기
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'dic_area')))
         
-        # JavaScript로 새 창이 열리는 링크를 수정
         driver.execute_script("document.querySelectorAll('a').forEach(link => { link.onclick = function(event) { event.preventDefault(); }; });")
     
-        # 페이지 소스 가져오기
         html = driver.page_source
         soup = BeautifulSoup(html, "html.parser")
 
-        # 콘텐츠 및 이미지 추출
-        content=soup.select_one('#dic_area').get_text()
-        image=soup.select_one('#img1')
-        if image :
+        content = soup.select_one('#dic_area').get_text()
+        image = soup.select_one('#img1')
+        if image:
             image_src = image.get('src')
             if image_src:
                 print(image_src)
             else:
                 print("No src attribute found")
-        else :
+        else:
             image_src = None
             print("No image found")
     except AttributeError as e:
         print("An error occurred:", e)
-    content=text_clean(content)
-    summarys.append(content)
+    content = text_clean(content)
+    contents.append(content)
     images.append(image_src)
+
 driver.quit()
 print("\n")
 
-print(type(nums), type(presses), type(wdates), type(articleURLs), type(titles), type(summarys), type(images))
+print(type(nums), type(presses), type(wdates), type(articleURLs), type(titles), type(contents), type(images))
 
-
-# db에 크롤링한 데이터 저장
-def to_dbeaver(nums ,presses, wdates, articleURLs, titles, summarys, images) :
+def to_dbeaver(nums, presses, wdates, articleURLs, titles, contents, images):
     conn = pymysql.connect(
         host='hostname'
         , user='username'
@@ -217,19 +196,17 @@ def to_dbeaver(nums ,presses, wdates, articleURLs, titles, summarys, images) :
     )
     cur = conn.cursor()
     
-    for num,press,wdate,url,title,summary,image in zip(nums, presses, wdates, articleURLs, titles, summarys, images):
-        num=int(num)
-        sql = "INSERT INTO news (Number, Press, Wdate, Url, Title, Summary, Image) VALUES ({}, '{}', '{}', '{}', '{}', '{}', '{}')".format(
-                num, press, wdate, url, title, summary, image)
-        try : 
+    for num, press, wdate, url, title, content, image in zip(nums, presses, wdates, articleURLs, titles, contents, images):
+        num = int(num)
+        sql = "INSERT INTO news (Number, Press, Wdate, Url, Title, Contents, Image) VALUES ({}, '{}', '{}', '{}', '{}', '{}', '{}')".format(
+                num, press, wdate, url, title, content, image)
+        try: 
             cur.execute(sql)
             print('insert to DB Success!')
-        except Exception as e :
+        except Exception as e:
             print('Error Message :', e)
             pass
     conn.commit()
     conn.close()
 
-#to_dbeaver(nums, presses, wdates, articleURLs, titles, summarys, images)
-
-
+to_dbeaver(nums, presses, wdates, articleURLs, titles, contents, images)
